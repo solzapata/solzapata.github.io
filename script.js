7c121dacf648592209b1927797e92b05c74b7582
solzapata <sol.zapata7@gmail.com>
2020-06-23 19:47:57 -0300

    agrego scroll

---
+++ script.js
@@ -0,0 +1,20 @@
+// ANIMACIÓN PARA UN SMOOTH SCROLL
+$(document).ready(() => {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
